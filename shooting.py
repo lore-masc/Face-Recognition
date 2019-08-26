03784e3ff9393b34dba11a3669eda3e99dd5dd06
Donato <donato.maragno.da@gmail.com>
2019-08-26 13:10:58 +0200

    shooting script
    only for frontal images

---
+++ shooting.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+cap = cv2.VideoCapture(0)
+i = 29
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        if w == 228:
+            i += 1
+            img_item = str(i) + "my-image.png"
+            cv2.imwrite("Angelo/" + img_item, roi_color)
+            cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+        color = (255, 100, 50)
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
